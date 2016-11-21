d8939570e11033a95763ebc9db73749ecf823c5b
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-21 16:56:39 +0000

    bot up and running

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express')
 var app = express()
 
@@ -8,3 +10,31 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+var Botkit = require('./node_modules/botkit/lib/Botkit.js');
+
+function get_response(){
+  var responses = [
+    'There was a car coming.',
+    'To get to the other side.',
+    'To get the newspaper.',
+    'Because it wanted to find out what those jokes were about.',
+    'To boldly go where no chicken has gone before!',
+    'Because the light was green.',
+    'I could tell you, but then the Chicken Mafia would kill me.'
+  ];
+
+  return responses[Math.floor(Math.random() * responses.length)];
+}
+
+var controller = Botkit.slackbot({
+  debug: false
+});
+
+var bot = controller.spawn({
+  token: process.env.SLACK_KEY
+}).startRTM();
+
+controller.hears(['why did the chicken cross the road'], 'direct_message,direct_mention,mention', function(bot, message) {
+  bot.reply(message, get_response());
+});

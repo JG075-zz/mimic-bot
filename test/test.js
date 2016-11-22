3d95b979afd616df8b4aeb3438325d81f8e0abc8
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-22 17:06:30 +0000

    pull text from tweet and store in array

--- test/test.js
+++ test/test.js
@@ -4,13 +4,28 @@ var Tweets = require('../lib/tweets').Tweets;
 var TwitterAPI = require('../lib/twitterAPI').TwitterAPI;
 
 describe('Tweets', function() {
+  beforeEach(function(){
+    Twitter = new TwitterAPI();
+  });
     it('should call our TweetAPI object', function() {
-      var Twitter = new TwitterAPI();
-
       sinon.spy(Twitter, "getRequest");
       var tweets = new Tweets(Twitter);
       tweets.getTweets();
       sinon.assert.calledOnce(Twitter.getRequest);
     });
 
+    it('should add tweets to tweets array', function(){
+      var twitterAPISample = {
+  "statuses": [
+    {
+      "text": "Aggressive Ponytail #freebandnames"
+    }]};
+      sinon.stub(Twitter, "getRequest", function(fn){
+        fn(twitterAPISample);
+      });
+      var tweets = new Tweets(Twitter);
+      tweets.getTweets();
+      console.log(tweets.tweets.length);
+      assert.equal(tweets.tweets.length, 1);
+    });
 });

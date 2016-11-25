1fa9b1d385b33c21fcee0c44d9de2cbd115230c5
Dagmara <daga.sz@hotmail.com>
2016-11-25 10:52:43 +0000

    Fixing port

--- app.js
+++ app.js
@@ -12,16 +12,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-})
+app.listen(process.env.PORT || 3000, function () {});
 
 var Botkit = require('./node_modules/botkit/lib/Botkit.js');
 
 tweets.getTweets();
 
 function getResponse(){
-  console.log(3);
   return tweets.tweets[Math.floor(Math.random() * tweets.tweets.length)];
 }
 

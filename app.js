6fa12620aeb40d227ef789c0db8763bc2d86752a
ManuCiao <mn.sabatino@gmail.com>
2016-11-25 10:58:28 +0000

    Add a working Strawpoll

--- app.js
+++ app.js
@@ -55,3 +55,28 @@ controller.hears(['wall'], 'ambient', function(bot, message) {
   bot.reply(message, createMessage(getResponse(), getPersona()));
   }
 });
+
+
+var hyperquest = require('hyperquest');
+
+var URL = 'http://strawpoll.me/api/v2/polls';
+var HEADERS = {
+  'Content-Type': 'application/json;charset=UTF-8',
+  'X-Requested-With': 'node-strawpoll'
+};
+
+function create(options) {
+  var stream = hyperquest.post(URL, {headers: HEADERS});
+  stream.end(JSON.stringify(options));
+  return stream;
+}
+
+function get(id) {
+  var stream = hyperquest.get(URL + '/' + id);
+  return stream;
+}
+
+create.create = create;
+create.get = get;
+
+module.exports = create;

b7912509dcc62d52f5164549dc74b9e28e0616cb
Dagmara <daga.sz@hotmail.com>
2016-11-25 10:37:35 +0000

    Express is back

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ var TwitterAPI = require('./lib/twitterAPI').TwitterAPI;
 var twitterAPI = new TwitterAPI();
 var tweets = new Tweets(twitterAPI, ["CIA", "Farage"]);
 
-// var express = require('express');
-// var app = express();
-//
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-//
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// })
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})
 
 var Botkit = require('./node_modules/botkit/lib/Botkit.js');
 

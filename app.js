98da4dff97a4635ea397a4583b23dc4d4e7bd97f
James Gardiner <jamesgardiner075@gmail.com>
2017-02-09 19:23:09 +0000

    Make StrawPoll optional and clean up functions

--- app.js
+++ app.js
@@ -7,75 +7,59 @@ var Tweets = require('./lib/tweets').Tweets;
 var TwitterAPI = require('./lib/twitterAPI').TwitterAPI;
 var StrawPollAPI = require('./lib/strawpollAPI').StrawPollAPI;
 
-var twitterAPI;
-var tweets;
+var twitterAPI = new TwitterAPI('realDonaldTrump');
+var tweets = new Tweets(twitterAPI, ["the", "a", "them", "guys"]);
+var useStrawPoll = true;
 var image;
 
-function createAndPostPoll() {
-  StrawPollAPI.createPoll();
-  StrawPollAPI.postPoll();
-}
-
-function retrieveWinnerFromPoll() {
-    //StrawPollAPI.retrievePoll();
-    setTimeout(function(){
-      // StrawPollAPI.getNamePoll();
-      // StrawPollAPI.getWinner();
-      // StrawPollAPI.getWinnerUsername();
-      // twitterAPI = new TwitterAPI(StrawPollAPI.winnerName);
-      twitterAPI = new TwitterAPI('realDonaldTrump');
-      twitterAPI.getImage(function(response){
-        image = response;
-      });
+if (useStrawPoll) {
+  StrawPollAPI.startPoll();
+  setTimeout(function(){
+    StrawPollAPI.retrievePoll(function(){
+      twitterAPI = new TwitterAPI(StrawPollAPI.winnerName);
       tweets = new Tweets(twitterAPI, ["the", "a", "them", "guys"]);
-      console.log("ready!");
-      // console.log(StrawPollAPI.winnerName);
-      tweets.getTweets();
-    }, 6000);
+      createBot();
+    });
+  }, 20000);
+} else {
+  createBot();
 }
 
-// For every poll winner=-0§1 ``
-// Create a new Tweets object
-// Pass in the name
-// Call get tweets
-// Then do everything else
+function createBot() {
 
-function getResponseText(){
-  return tweets.tweets[Math.floor(Math.random() * tweets.tweets.length)];
-}
+  console.log('Creating the bot!');
 
-function createResponse(responseText, persona){
-  return {
-    text: responseText,
-    username: persona.name,
-    icon_url: persona.icon
-  };
-}
+  twitterAPI.getImage(function(response){image = response;});
+  tweets.getTweets();
 
-function getResponsePersona(username, image){
-  return {
-    name: username || 'bob',
-    icon: image || "http://images2.fanpop.com/images/photos/7000000/Spongebob-Icon-spongebob-squarepants-7039737-100-100.jpg"
-  };
-}
-exports.getResponsePersona = getResponsePersona;
-
-var controller = Botkit.slackbot({
-  debug: false
-});
-
-
-var bot = controller.spawn({
-  token: process.env.SLACK_KEY
-}).startRTM();
-
-controller.hears(['mimic'], 'ambient', function(bot, message) {
-  if (tweets.tweets.length > 0) {
-  bot.reply(message, createResponse(getResponseText(), getResponsePersona(StrawPollAPI.winnerName, image)));
+  function getResponseText(){
+    return tweets.tweets[Math.floor(Math.random() * tweets.tweets.length)];
   }
-});
 
-//createAndPostPoll();
-setTimeout(function(){
-  retrieveWinnerFromPoll();
-}, 10);
+  function createResponse(responseText, persona){
+    return {
+      text: responseText,
+      username: persona.name,
+      icon_url: persona.icon
+    };
+  }
+
+  function getResponsePersona(username, image){
+    return {
+      name: username || 'bob',
+      icon: image || "http://images2.fanpop.com/images/photos/7000000/Spongebob-Icon-spongebob-squarepants-7039737-100-100.jpg"
+    };
+  }
+
+  var controller = Botkit.slackbot({ debug: false });
+  var bot = controller.spawn({ token: process.env.SLACK_KEY }).startRTM();
+
+  controller.hears(['mimic'], 'ambient', function(bot, message) {
+    if (tweets.tweets.length > 0) {
+      bot.reply(message, createResponse(getResponseText(), getResponsePersona(StrawPollAPI.winnerName, image)));
+    }
+  });
+
+  exports.getResponsePersona = getResponsePersona;
+
+}

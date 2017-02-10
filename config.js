02da5173dfc8e7e1ddfebe2986fe4ce1e1a39752
James Gardiner <jamesgardiner075@gmail.com>
2017-02-10 22:50:21 +0000

    Remove unused files

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
-
-module.exports = config;
